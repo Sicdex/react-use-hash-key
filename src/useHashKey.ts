f86661d6632d7be9dbc57432f3cfe448f9be9362
SicdeX <sicdex@gmail.com>
2022-11-15 22:44:34 +0600

    see desc
    - fixed NaN
    - add custom prefix

--- src/useHashKey.ts
+++ src/useHashKey.ts
@@ -2,21 +2,28 @@ import { useId } from "react"
 
 /**
  * @param key Number | String
- * @param useRId Boolean | default = true | use useId() React 18+
- * @param digits Number | default = 1 | if use number key 0.123 flag digits convert toFixed(digits: 1)
+ * @param prefix String | Undefined | Default = useId() - React 18+
+ * @param digits Number | Default = 1 | If use number key 0.123 flag digits convert toFixed(digits: 1)
  * @returns Number 32bit integer
  * @example arr.map(ent => (<Component key={useHashKey(ent.title)})))
  */
-export const useHashKey = (key: string | number, useRId = true, digits = 1) => {
+export const useHashKey = (key: string | number, prefix = useId(), digits = 1) => {
+
     let hash = 0, i: number, chr: number
-    const str = typeof key === "number" ? (key << 1).toFixed(digits) : key
+
+    const str = (typeof key !== "number" 
+        ? key
+        : key.toFixed(digits)) + (prefix ?? "none")
+
     if (str.length === 0) {
         return hash
     }
+
     for (i = 0; i < str.length; i++) {
-        chr = ((useRId ? useId() : "") + ":" + str).charCodeAt(i)
+        chr = str.charCodeAt(i)
         hash = (hash << 5) - hash + chr
         hash |= 0
     }
+
     return hash
 }
